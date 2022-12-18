ece1cb782ac11b7804349db61c7e73821fa458fd
onurdemirkale <onurdemirkale@protonmail.com>
2022-12-19 01:25:10 +0300

    Dummy update to trigger pipeline

--- handler.ts
+++ handler.ts
@@ -1,5 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 async function main(event: any) {
+  console.log("Hello!");
   console.log("Region: ", process.env.REGION);
   console.log("Availability zones: ", process.env.AVAILABILITY_ZONES);
 
